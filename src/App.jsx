5a427aaa21885a395f7af123803e03691c3882f6
DarshakHemjani <dasuhemjani@gail.com>
2024-04-11 18:39:12 +0530

    Router in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignOut from "./pages/SignOut";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+
 export default function App() {
   return (
-    <div className="text-red-500">
-      App
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signOut" element={<SignOut />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
