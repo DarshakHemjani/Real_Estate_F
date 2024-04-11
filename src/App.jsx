6badcbe1173f1b0c74e73d4b9b3e205113dd5a76
DarshakHemjani <dasuhemjani@gail.com>
2024-04-12 02:53:53 +0530

    Signi Up page UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import SignOut from "./pages/SignOut";
+import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
@@ -15,7 +15,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/signin" element={<SignIn />} />
-        <Route path="/signOut" element={<SignOut />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
