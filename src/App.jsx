aab37e08c7c905990dd438bf7da838a3f1a706fd
DarshakHemjani <dasuhemjani@gail.com>
2024-04-11 18:49:09 +0530

    Header Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import SignOut from "./pages/SignOut";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
+import Headers from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Headers />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
